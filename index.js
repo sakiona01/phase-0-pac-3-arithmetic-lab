c19302e7a9c394001c8634ce313f6b885f1a82bb
shawna <shawna.akiona@gmail.com>
2021-06-16 15:43:35 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    var result= a+b;
+    return result;
+}
+
+function subtract(a, b) {
+    var result= a-b;
+    return result;
+}
+
+function multiply(a, b) {
+    var result= a*b;
+    return result;
+}
+
+function divide(a, b) {
+    var result= a/b;
+    return result;
+}
+
+function increment(n) {
+    var result= n+1;
+    return result;
+}
+
+function decrement(n) {
+    var result= n-1;
+    return result;
+}
+
+function makeInt(n) {
+    var result= parseInt(n, 10);
+    return result;
+}
+
+function preserveDecimal(n) {
+    var result= parseFloat(n);
+    return result;
+}
